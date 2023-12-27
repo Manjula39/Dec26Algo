c1934e7248f10f593a119f9148708c90d26467a8
Manjula36 <manjulavdt@gmail.com>
2023-12-27 15:00:50 +0530

    Add project files.

---
+++ Dec26Algo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;//root or base namespace
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dec26Algo
+{
+    internal class Program
+    {
+        static void BSort(int[] arr)
+        {
+            int temp = 0;
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        static void PrintAfterBSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 78, 55, 45, 98, 13, 3 };
+            int temp = 0;
+            for (int i = 0; i <= arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("after buble sort");
+            foreach (int x in arr) Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Dec26Algo/SelectionSort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dec26Algo
+{
+    internal class SelectionSort
+    {
+        static void SelectSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int ind_min = 1;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[ind_min])
+                    {
+                        ind_min = j;
+                        int temp = arr[ind_min];
+                        arr[ind_min] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+
+        }
+
+        static void PrintSelectSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+            Console.WriteLine("\n");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("testing ");
+            int[] nums = { 4, 2, 1, 8, 3 };
+            SelectSort(nums);
+            PrintSelectSort(nums);
+        }
+    }
+
+
+
+        }
